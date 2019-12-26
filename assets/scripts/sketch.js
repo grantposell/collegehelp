9767be140e4a732403fd6874b88b951ff9d3c64b
grantposell <grantposell@gmail.com>
2019-12-26 16:33:58 -0600

    Nearly done with ajax call but facing some issues.

--- assets/scripts/sketch.js
+++ assets/scripts/sketch.js
@@ -1,11 +1,30 @@
-function setup(){
 
+$(document).on("click", ".survey", function (event) {
+    // console.log($(this))
+    event.preventDefault();
+    var zip = $("#zipInput").val().trim();
+
+    console.log(zip)
+    zipcodeRadius(zip);
+});
+function zipcodeRadius(zip) {
+    var url = "https://www.zipcodeapi.com/rest/vbTp6aLEGsVTTrt1MtcFQuSJ8kjKMJpBZjzb5uSrn2sNXwa6WTcdt2GxWfretPXL/radius.json/" + zip + "/10/mile";
+    console.log(url);
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'json',
+    }).then(function (response) {
+        console.log(response)
+    });
+}
+function setup() {
     loadJSON("./assets/scripts/data.json", gotData);
+
 }
 function gotData(data) {
     console.log(data)
 }
-
-function draw(){
+function draw() {
 
 }
