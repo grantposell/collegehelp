9586e9a503f61cadd66383670933f05b6113a955
grantposell <grantposell@gmail.com>
2020-01-04 22:55:07 -0600

    Added more pages and more logic for REST API for zipcodes

--- assets/scripts/sketch.js
+++ assets/scripts/sketch.js
@@ -1,4 +1,25 @@
+const api = 'https://www.zipcodeapi.com/rest/vbTp6aLEGsVTTrt1MtcFQuSJ8kjKMJpBZjzb5uSrn2sNXwa6WTcdt2GxWfretPXL/radius.json/';
 
+
+
+$(document).on("click", ".survey", function (event) {
+    var zip = $("#zipInput").val().trim();
+    let uri = api + zip + '/10/mile';
+    event.preventDefault();
+    function getCheckout() {
+        fetch(uri, {
+            headers: new Headers({
+                //In a more developed application I would put this in a dotenv file
+                'Access-Control-Allow-Origin': 'https://www.zipcodeapi.com/rest/vbTp6aLEGsVTTrt1MtcFQuSJ8kjKMJpBZjzb5uSrn2sNXwa6WTcdt2GxWfretPXL/radius.json/'
+                
+            })
+        })
+            .then((res) => res.json())
+            .then((data) => {
+            })
+    }
+    getCheckout();
+});
 function setup() {
     loadJSON("../scripts/data.json", gotData);
 
